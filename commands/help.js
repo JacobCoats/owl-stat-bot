7a705b4df465d175aa719cb255aa63a146774d4b
Jacob Coats <tjcoats97@gmail.com>
2019-03-06 19:10:57 -0600

    Changed to single quotes in help.js
    Changed to single quotes in help.js for consistency

--- commands/help.js
+++ commands/help.js
@@ -1,3 +1,3 @@
 exports.run = (params, message) => {
-    message.channel.send("You can find help in the bot's documentation at the following link: \n\nhttps://github.com/JacobCoats/owl-stat-bot/blob/master/README.md");
+    message.channel.send('You can find help in the bot\'s documentation at the following link: \n\nhttps://github.com/JacobCoats/owl-stat-bot/blob/master/README.md');
 }
