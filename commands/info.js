ad2666c519ac574bf16f5c96caccb069bb84c7f9
Jacob Coats <tjcoats97@gmail.com>
2019-03-06 18:56:47 -0600

    Added info command
    Added info command which returns information about the bot and a link to the source code

---
+++ commands/info.js
@@ -0,0 +1,4 @@
+exports.run = (params, message) => {
+    message.channel.send('This bot was created by **Jacob Coats** using the discord.js module for Node.js. ' +
+                         'You can find a link to the source code here: \n\nhttps://github.com/JacobCoats/owl-stat-bot/');
+}
