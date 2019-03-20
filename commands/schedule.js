dbfdfec35c999337751387692e32ceb8401ab28d
Jacob Coats <tjcoats97@gmail.com>
2019-03-20 00:34:17 -0500

    Fixed comment in schedule.js
    - Fixed an inaccurate comment in schedule.js
    - Updated version number in package.json

--- commands/schedule.js
+++ commands/schedule.js
@@ -98,7 +98,7 @@ function sendCurrentWeek(message, offset, teamId) {
                     week = j;
                     break;
                 }
-                // If the current week is between stages, set the last week played to current
+                // If the current week is between stages, set the first week of the next stage to current
                 if (j === 4) {
                     if (currentDate > body['data']['stages'][`${i}`]['weeks'][`${j}`]['endDate'] &&
                         currentDate < body['data']['stages'][`${i + 1}`]['weeks'][`${0}`]['endDate']) {
