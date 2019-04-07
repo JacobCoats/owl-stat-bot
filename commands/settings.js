d339504a1971600b77e7382a1c469ddbe37628bf
Jacob Coats <tjcoats97@gmail.com>
2019-04-07 16:44:10 -0500

    Revert "Updated comments"
    This reverts commit 6280a3029fe6d20994e74eca8b4ca5d17943b701.

--- commands/settings.js
+++ commands/settings.js
@@ -2,7 +2,6 @@ const guildSettings = require('../guild-settings.json');
 const fs = require('fs');
 
 exports.run = (params, message) => {
-    // Decide which method to call based on the parameter that the user specified
     if (params[0].toLowerCase() === 'prefix') {
         if (params[1]) {
             changePrefix(message, params[1]);
@@ -25,13 +24,11 @@ exports.run = (params, message) => {
 }
 
 function changePrefix(message, newPrefix) {
-    // If the user is not an administrator, they can't change the prefix
     if (!message.member.permissions.has('ADMINISTRATOR')) {
         message.channel.send("Error: only server administrators can change the server prefix");
         return;
     }
 
-    // Change the server's prefix in guild-settings.json and write it to the file
     guildSettings[message.guild.id].prefix = newPrefix;
     fs.writeFile('./guild-settings.json', JSON.stringify(guildSettings, null, 2), (err) => {
         if (err) {
@@ -43,19 +40,16 @@ function changePrefix(message, newPrefix) {
 }
 
 function enableCommand(message, commandToEnable) {
-    // If the user is not an administrator, they can't enable the command
     if (!message.member.permissions.has('ADMINISTRATOR')) {
         message.channel.send('Error: only server administrators can enable commands');
         return;
     }
 
-    // Check to make sure that the specified command exists
     if (!commands.includes(commandToEnable)) {
         message.channel.send('Error: that command doesn\'t exist');
         return;
     }
 
-    // If the command is disabled, enable it
     if (guildSettings[message.guild.id].disabled.includes(commandToEnable)) {
         guildSettings[message.guild.id].disabled.splice(
             guildSettings[message.guild.id].disabled.indexOf(commandToEnable), 1
@@ -65,7 +59,6 @@ function enableCommand(message, commandToEnable) {
         return;
     }
 
-    // Write changes to guild-settings.json
     fs.writeFile('./guild-settings.json', JSON.stringify(guildSettings, null, 2), (err) => {
         if (err) {
             console.log('Error enabling command in guildSettings.json: ' + err);
@@ -76,19 +69,16 @@ function enableCommand(message, commandToEnable) {
 }
 
 function disableCommand(message, commandToDisable) {
-    // If the user is not an administrator, they can't disable the command
     if (!message.member.permissions.has('ADMINISTRATOR')) {
         message.channel.send('Error: only server administrators can disable commands');
         return;
     }
 
-    // Check to make sure that the specified command exists
     if (!commands.includes(commandToDisable)) {
         message.channel.send('Error: that command doesn\'t exist');
         return;
     }
 
-    // If the command is enabled, disable it
     if (guildSettings[message.guild.id].disabled.includes(commandToDisable)) {
         message.channel.send('Error: that command is already disabled');
         return;
@@ -96,7 +86,6 @@ function disableCommand(message, commandToDisable) {
         guildSettings[message.guild.id].disabled.push(commandToDisable);
     }
 
-    // Write changes to guild-settings.json
     fs.writeFile('./guild-settings.json', JSON.stringify(guildSettings, null, 2), (err) => {
         if (err) {
             console.log('Error disabling command in guildSettings.json: ' + err);
@@ -106,7 +95,6 @@ function disableCommand(message, commandToDisable) {
     });
 }
 
-// A list of all of the available commands
 let commands = [
     "help",
     "info",
